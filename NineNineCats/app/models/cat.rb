104cf5d0a4d5755acf8a9fdebbca82935ae48b53
mrkchoi <kennethichoi@gmail.com>
2019-06-04 14:21:27 -0700

    Add Cat model level validations

--- NineNineCats/app/models/cat.rb
+++ NineNineCats/app/models/cat.rb
@@ -1,3 +1,28 @@
+require 'date'
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  birth_date  :date             not null
+#  color       :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Cat < ApplicationRecord
+  COLORS = ["brown", "orange", "black", "blue"]
   
+  validates :name, :birth_date, :color, :sex, :description, presence: true
+  validates :color, inclusion: { in: COLORS }
+  validates :sex, inclusion: { in: %w(M, F) }
+
+  def age
+    DateTime.now.year - self.birth_date.year
+  end
+
+
 end
--- NineNineCats/test/models/cat_test.rb
+++ NineNineCats/test/models/cat_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  birth_date  :date             not null
+#  color       :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class CatTest < ActiveSupport::TestCase
